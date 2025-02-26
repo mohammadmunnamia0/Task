843a63d9632c83e61f127833d7e3cda9510cd452
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-02-26 11:30:06 +0600

    Deployment updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import {
   RouterProvider,
 } from "react-router-dom";
-import { router } from "./routes/routes";
+import { router } from "./Routes/Routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
